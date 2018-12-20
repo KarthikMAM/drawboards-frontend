60a5148b565565ee9671285f4cb951d946c93faf
Karthik M A M <karthik_m_a_m@outlook.com>
2018-12-20 10:55:07 +0530

    update src files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Canvas from './components/canvas'
-import './index.scss'
+import AWSAppSyncClient from 'aws-appsync'
+import { ApolloProvider } from 'react-apollo'
+import { Rehydrated } from 'aws-appsync-react'
 
-ReactDOM.render(<Canvas />, document.getElementById('root'))
+import './styles/index.scss'
+import DrawboardsPage from './containers/DrawboardsPage'
+
+const client = new AWSAppSyncClient({
+  url: process.env.REACT_APP_GRAPHQL_URL,
+  region: process.env.REACT_APP_AWS_REGION,
+  auth: {
+    type: 'API_KEY',
+    apiKey: process.env.REACT_APP_API_KEY,
+  },
+})
+
+ReactDOM.render((
+  <ApolloProvider client={client}>
+    <Rehydrated>
+      <DrawboardsPage />
+    </Rehydrated>
+  </ApolloProvider>
+), document.getElementById('root'))
