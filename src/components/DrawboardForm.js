e4e61598d1681a86c6a8803ecfbe8782892ee4c3
Karthik M A M <karthik_m_a_m@outlook.com>
2018-12-20 10:49:30 +0530

    add drawboard form

---
+++ src/components/DrawboardForm.js
@@ -0,0 +1,29 @@
+import React, { PureComponent } from 'react'
+
+import Input from './Input'
+import File from './FileInput'
+import Styles from './styles/DrawboardForm.module.scss'
+
+export default class DrawboardForm extends PureComponent {
+  render () {
+    return (
+      <form className={Styles.Form} onSubmit={this.props.onSubmit}>
+        <Input
+          required
+          type='text'
+          label='Title'
+          name='title'
+          value={this.props.value.title}
+          onChange={this.props.onChange} />
+        <File
+          required
+          value={this.props.value.image}
+          name='image'
+          label='Background'
+          accept='image/jpeg,image/jpg'
+          onChange={this.props.onChange} />
+        <Input required type='submit' />
+      </form>
+    )
+  }
+}
